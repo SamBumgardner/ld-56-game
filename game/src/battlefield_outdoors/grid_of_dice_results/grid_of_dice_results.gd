5db822276a00ee89d261c76a2026f5ecd060b44e
Noah Bumgardner <noah@playgamecafe.com>
2024-10-05 13:40:39 -0500

    Increase maximum Die Slots from 5 to 9

--- game/src/battlefield_outdoors/grid_of_dice_results/grid_of_dice_results.gd
+++ game/src/battlefield_outdoors/grid_of_dice_results/grid_of_dice_results.gd
@@ -2,18 +2,23 @@ extends GridContainer
 
 var character_die_slots: Array[CharacterDieSlot] = []
 
-func _ready():
-    character_die_slots = []
+# Should match `get_child_count()`.
+var maximum_slots = 9
 
-    for hired_character_index in Database.hired_characters.size():
-        character_die_slots.append(
-            CharacterDieSlot.new(
-                Database.hired_characters[hired_character_index]
-            )
-        )
+func _ready():
+    _initialize_character_die_slots(Database.hired_characters)
+
 
 func roll_dice() -> void:
     for character_die_slot_index in character_die_slots.size():
+        if character_die_slot_index >= maximum_slots:
+            print_debug(
+                'Maximum die slots of ',
+                maximum_slots,
+                ' exceeded, stopping rolls.'
+            )
+            return
+
         var character_die_slot: CharacterDieSlot = character_die_slots[
             character_die_slot_index
         ]
@@ -23,4 +28,19 @@ func roll_dice() -> void:
 
         character_die_slot.last_roll_result = character_die_slot.roll_action()
 
-        get_child(character_die_slot_index).set_action(character_die_slot.last_roll_result)
+        get_child(
+            character_die_slot_index
+        ).set_action(
+            character_die_slot.last_roll_result
+        )
+
+
+func _initialize_character_die_slots(characters) -> void:
+    character_die_slots = []
+
+    for hired_character_index in characters.size():
+        character_die_slots.append(
+            CharacterDieSlot.new(
+                characters[hired_character_index]
+            )
+        )
