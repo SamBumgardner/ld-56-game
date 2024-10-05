c7e3eb97091b0b60bf14b2b4827c69c0f509bcef
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-05 08:02:26 -0500

    add light documentation to upgrades to explain concept.

--- game/src/characters/UpgradeChoice.gd
+++ game/src/characters/UpgradeChoice.gd
@@ -1,5 +1,12 @@
 class_name UpgradeChoice extends Resource
 
+## Keys for the upgrade_funcs dictionary. Need one for every type of upgrade operation.
+enum UpgradeType {
+    REPLACE_ONE,
+}
+
+## Add methods to this dictionary keyed by their unique UpgradeType.
+## This lets us translate an editor-set upgrade_type into a code action.
 static var upgrade_funcs: Dictionary = {
     UpgradeType.REPLACE_ONE: _upgrade_replace_one
 }
@@ -13,12 +20,10 @@ static var upgrade_funcs: Dictionary = {
 @export var remove_action_name: String
 @export var new_action: Action
 
-enum UpgradeType {
-    REPLACE_ONE,
-}
-
 func apply_changes(action_selector: ActionSelector) -> void:
     upgrade_funcs[upgrade_type].call(self, action_selector)
 
+## Example upgrade function. All others should have same signature so they can be used
+## interchangeably.
 static func _upgrade_replace_one(upgrade: UpgradeChoice, action_selector: ActionSelector):
     action_selector.replace(upgrade.remove_action_name, upgrade.new_action)
