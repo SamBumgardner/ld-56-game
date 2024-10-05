148a53ca34a023ea1f1b93f0066fedfb653fc334
Noah Bumgardner <noah@playgamecafe.com>
2024-10-04 23:19:21 -0500

    Show Warning When Fail to Reroll

--- game/src/battlefield_outdoors_hud/battlefield_outdoors_hud.gd
+++ game/src/battlefield_outdoors_hud/battlefield_outdoors_hud.gd
@@ -2,15 +2,21 @@ extends Control
 
 @onready var health_current = $MarginContainer/HealthTrackerRow/HealthCurrent
 @onready var health_maximum = $MarginContainer/HealthTrackerRow/HealthMaximum
+@onready var warning_out_of_troops = (
+	$MarginContainer2/VBoxContainer/WarningOutOfTroops
+)
 
 var mock_damage_amount = 21
 
 
 func _ready():
+	warning_out_of_troops.visible = false
 	_set_health_text()
 
 
 func _on_mock_attack_button_pressed():
+	warning_out_of_troops.visible = false
+
 	Database.set_war_transport_health_current(
 		Database.war_transport_health_current
 		- mock_damage_amount
@@ -20,3 +26,8 @@ func _on_mock_attack_button_pressed():
 func _set_health_text():
 	health_current.text = str(Database.war_transport_health_current)
 	health_maximum.text = str(Database.war_transport_health_maximum)
+
+
+func _on_mock_reroll_button_pressed():
+	print_debug('Player requested a roll without any troops.')
+	warning_out_of_troops.visible = true
