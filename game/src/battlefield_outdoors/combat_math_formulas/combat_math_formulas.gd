c0ce04cca4c36b27fd4b83fbbfa4ba8c730b1b47
Noah Bumgardner <noah@playgamecafe.com>
2024-10-05 17:04:07 -0500

    Encapsulate Looped Code into Private Methods
    (cherry picked from commit 18eeb50ef45cc7a8f4aaccde996987a1ce09b6dc)

--- game/src/battlefield_outdoors/combat_math_formulas/combat_math_formulas.gd
+++ game/src/battlefield_outdoors/combat_math_formulas/combat_math_formulas.gd
@@ -9,32 +9,49 @@ func sum_dice_amounts(
     character_die_slots: Array[CharacterDieSlot]
 ) -> int:
     # Type `Array[Action]`.
-    var character_die_results = (
-        character_die_slots.map(
-            func (character_die_slot: CharacterDieSlot) -> Action: return (
-                character_die_slot.last_roll_result
-            )
-        )
+    var populated_character_die_results = _get_populated_die_rolls(
+        character_die_slots
     )
 
     # Type `Array[int]`.
-    var character_die_results_amount_list = (
-        character_die_results
-            .filter(
-                func (character_die_result: Action) -> bool: return (
-                    character_die_result != null
-                )
-            )
-            .map(
-                func (character_die_result: Action) -> int: \
-                    return character_die_result.amount
-            )
+    var character_die_results_amount_list = populated_character_die_results.map(
+        _get_action_amount
     )
 
     return character_die_results_amount_list.reduce(
-        func (accumulator: int, amount: int) -> int: return (
-            accumulator
-            + amount
-        ),
+        _sum_integers,
         default_damage_amount
     )
+
+
+#region Private methods
+
+func _get_action_amount(action: Action) -> int:
+    return action.amount
+
+
+func _get_character_die_slot_last_roll_result(
+    character_die_slot: CharacterDieSlot
+) -> Action:
+    return character_die_slot.last_roll_result
+
+
+# Type `Array[CharacterDieSlot] -> Array[Action]`.
+func _get_populated_die_rolls(
+    character_die_slots: Array[CharacterDieSlot]
+) -> Array:
+    var character_die_results = character_die_slots.map(
+        _get_character_die_slot_last_roll_result
+    )
+
+    return character_die_results.filter(_is_action_populated)
+
+
+func _is_action_populated(action: Action) -> bool:
+    return action != null
+
+
+func _sum_integers(accumulator: int, amount: int) -> int:
+    return accumulator + amount
+
+#endregion Private methods
