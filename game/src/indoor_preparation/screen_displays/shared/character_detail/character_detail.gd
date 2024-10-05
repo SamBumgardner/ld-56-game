e0e3ab8aa0ca9df5f7e34ac3ab4dc59f886a8016
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-05 17:07:57 -0500

    styling (placeholder) applied for upgrade buttons

--- game/src/indoor_preparation/screen_displays/shared/character_detail/character_detail.gd
+++ game/src/indoor_preparation/screen_displays/shared/character_detail/character_detail.gd
@@ -4,6 +4,7 @@ class_name CharacterDetail extends Control
 @onready var character_summary: CharacterDetailSummary = $BasicSummary
 @onready var upgrade_selection: CharacterDetailUpgrades = $UpgradeSelection
 @onready var character_icon: TextureRect = $IconAndCost/CharacterIcon
+@onready var exit_button: Button = $ExitButton
 
 func _ready() -> void:
     #if self == get_tree().current_scene:
--- game/src/indoor_preparation/screen_displays/shared/upgrade_choice_display.gd
+++ game/src/indoor_preparation/screen_displays/shared/upgrade_choice_display.gd
@@ -1,12 +1,12 @@
 class_name UpgradeChoiceDisplay extends PanelContainer
 
-@onready var upgrade_button_1: TextureButton = $HBoxContainer/TextureButton
+@onready var upgrade_button_1: Button = $HBoxContainer/Button
 @onready var or_label: Label = $HBoxContainer/Label
-@onready var upgrade_button_2: TextureButton = $HBoxContainer/TextureButton2
+@onready var upgrade_button_2: Button = $HBoxContainer/Button2
 
 func set_upgrade_choice_data(upgrade_selector: UpgradeSelector) -> void:
-    upgrade_button_1.texture_normal = upgrade_selector.get_choice_data(0).icon_ref
-    upgrade_button_2.texture_normal = upgrade_selector.get_choice_data(1).icon_ref
+    upgrade_button_1.icon = upgrade_selector.get_choice_data(0).icon_ref
+    upgrade_button_2.icon = upgrade_selector.get_choice_data(1).icon_ref
     for child in get_children():
         child.show()
 
