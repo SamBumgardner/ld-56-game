750d24baa9d21db3322565f9a660b31315727402
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-05 22:05:02 -0500

    get hire preview data from characters

---
+++ game/src/indoor_preparation/screen_displays/browse_hires/new_hire_preview.gd
@@ -0,0 +1,33 @@
+class_name NewHirePreview extends MarginContainer
+
+const cost_format_string: String = "[center]Cost:\n[img=25%%]res://assets/art/ATTACK_icon_64x64.png[/img] %s"
+
+@onready var button: Button = $Button
+@onready var portrait: TextureRect = $MarginContainer/VBoxContainer/NewHirePreview/Portrait
+@onready var name_label: Label = $MarginContainer/VBoxContainer/NewHirePreview/VBoxContainer/Name
+@onready var description: Label = $MarginContainer/VBoxContainer/NewHirePreview/VBoxContainer/Description
+@onready var cost_label: RichTextLabel = $MarginContainer/VBoxContainer/HBoxContainer/Cost
+@onready var action_previews_1: Array[Node] = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/ActionsPreview.get_children()
+@onready var action_previews_2: Array[Node] = $MarginContainer/VBoxContainer/HBoxContainer/PanelContainer/MarginContainer/VBoxContainer/ActionsPreview2.get_children()
+
+var action_previews: Array[Node]
+
+func _ready() -> void:
+    action_previews = []
+    action_previews.append_array(action_previews_1)
+    action_previews.append_array(action_previews_2)
+
+func set_character_data(character: Character) -> void:
+    portrait.texture = character.icon
+    name_label.text = character.name
+    description.text = character.description
+    cost_label.text = cost_format_string % character.hiring_cost
+    _set_action_previews(character.actions.get_all())
+    
+func _set_action_previews(actions: Array[Action]) -> void:
+    for i in range(action_previews.size()):
+        if i < actions.size():
+            action_previews[i].texture = Database.stat_type_to_icon[actions[i].stat_type]
+            action_previews[i].show()
+        else:
+            action_previews[i].hide()
