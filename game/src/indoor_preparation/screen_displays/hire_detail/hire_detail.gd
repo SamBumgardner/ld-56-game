7a022adbf33629ee382153ff0909f9d6b79694fa
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-06 13:29:19 -0500

    fix accidental removal of hiredetail ready inheritance

--- game/src/indoor_preparation/screen_displays/hire_detail/hire_detail.gd
+++ game/src/indoor_preparation/screen_displays/hire_detail/hire_detail.gd
@@ -5,6 +5,7 @@ signal hire_purchase_pressed(character: Character)
 @onready var hire_cost_prompt: HirePrompt = $IconAndCost/HirePrompt
 
 func _ready() -> void:
+    super()
     hire_cost_prompt.purchase_button.pressed.connect(_on_hire_button_pressed)
 
 func set_character_data(new_character: Character):
