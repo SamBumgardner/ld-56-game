f330b36561d2835829877aa7e6841537994909df
Noah Bumgardner <noah@playgamecafe.com>
2024-10-12 08:09:14 -0500

    Add Button Sound Effects to Start Menu

--- game/src/audio_manager/audio_manager.gd
+++ game/src/audio_manager/audio_manager.gd
@@ -2,6 +2,10 @@
 extends Node
 
 
+@export var sfx_button_click : AudioStream
+@export var sfx_button_hover : AudioStream
+
+
 const _default_volumes: float = 0.5
 
 
@@ -14,3 +18,24 @@ func _ready():
     SoundManager.set_ambient_sound_volume(_default_volumes)
     SoundManager.set_music_volume(_default_volumes)
     SoundManager.set_sound_volume(_default_volumes)
+
+
+#region Button mouse entered
+
+func _on_quit_button_mouse_entered():
+    SoundManager.play_ui_sound(sfx_button_hover)
+
+func _on_start_button_mouse_entered():
+    SoundManager.play_ui_sound(sfx_button_hover)
+
+#endregion Button mouse entered
+
+#region Button press
+
+func _on_quit_button_pressed():
+    SoundManager.play_ui_sound(sfx_button_click)
+
+func _on_start_button_pressed():
+    SoundManager.play_ui_sound(sfx_button_click)
+
+#endregion Button press
