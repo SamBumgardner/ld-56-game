3cdef876444248c46845bf09342123afa75b1d99
Noah Bumgardner <noah@playgamecafe.com>
2024-10-12 08:59:50 -0500

    Add Settings Menu for Audio, Not Saved

---
+++ game/src/settings_menu/settings_menu.gd
@@ -0,0 +1,42 @@
+extends Control
+
+
+@onready var music_slider: Slider = $CenterContainer/VBoxContainer2/VBoxContainer/MusicVolumePercentageSlider
+@onready var sfx_slider: Slider = $CenterContainer/VBoxContainer2/VBoxContainer/SfxVolumePercentageSlider
+
+
+const setting_to_percentage_ratio = 100
+
+
+func _ready():
+    sfx_slider.value = int(
+        SoundManager.get_sound_volume() * setting_to_percentage_ratio
+    )
+    music_slider.value = int(
+        SoundManager.get_music_volume() * setting_to_percentage_ratio
+    )
+    print_debug(
+        'Slider values of SFX and music are: ',
+        sfx_slider.value,
+        music_slider.value
+    )
+
+
+func _on_music_volume_percentage_slider_drag_ended(value_changed):
+    if !value_changed:
+        return
+
+    SoundManager.set_sound_volume(
+        roundf(music_slider.value) / setting_to_percentage_ratio
+    )
+    print_debug('Updated music volume: ', SoundManager.get_sound_volume())
+
+
+func _on_sfx_volume_percentage_slider_drag_ended(value_changed):
+    if !value_changed:
+        return
+
+    SoundManager.set_sound_volume(
+        roundf(sfx_slider.value) / setting_to_percentage_ratio
+    )
+    print_debug('Updated sound volume: ', SoundManager.get_sound_volume())
--- game/src/start_menu/start_menu.gd
+++ game/src/start_menu/start_menu.gd
@@ -15,5 +15,9 @@ func _on_quit_button_pressed():
     get_tree().quit()
 
 
+func _on_settings_button_pressed():
+    get_tree().change_scene_to_file("res://src/settings_menu/SettingsMenu.tscn")
+
+
 func _on_start_button_pressed():
     get_tree().change_scene_to_file("res://src/gameplay/Gameplay.tscn")
