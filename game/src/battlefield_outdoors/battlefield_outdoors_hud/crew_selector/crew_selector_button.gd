a596bcb399367e3650148cbde2f3980031a8d953
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-11 22:29:46 -0500

    created crew selector button for left panel

---
+++ game/src/battlefield_outdoors/battlefield_outdoors_hud/crew_selector/crew_selector_button.gd
@@ -0,0 +1,22 @@
+class_name CrewSelectorButton extends TextureRect
+
+signal character_selected(character: Character)
+
+@onready var button: Button = $Button
+
+var character: Character
+
+func _ready() -> void:
+    button.pressed.connect(_on_button_pressed)
+    set_character(character)
+
+func set_character(new_character: Character) -> void:
+    character = new_character
+    if character != null:
+        texture = character.icon
+        show()
+    else:
+        hide()
+
+func _on_button_pressed() -> void:
+    character_selected.emit()
