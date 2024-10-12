676056d10ffc20b58d6f4aa85ed084a78c880601
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-11 21:27:16 -0500

    enable freezing by right clicking

--- game/src/shared_ui/character_action_display/character_action_display.gd
+++ game/src/shared_ui/character_action_display/character_action_display.gd
@@ -1,8 +1,14 @@
 class_name CharacterActionDisplay extends TextureRect
 
 @onready var die_result: DieResult = $DieResult
+@onready var frozen_background: Control = $FrozenBackground
+@onready var frozen_icon: TextureRect = $FrozenIcon
+@onready var button: Button = $Button
 
 var character_die_slot: CharacterDieSlot
+
+func _ready() -> void:
+    button.gui_input.connect(_handle_freeze_roll_action)
 
 func set_character_die_slot(new_die_slot: CharacterDieSlot) -> void:
     character_die_slot = new_die_slot
@@ -15,4 +21,14 @@ func set_character_die_slot(new_die_slot: CharacterDieSlot) -> void:
 
 func toggle_freeze() -> void:
     character_die_slot.is_frozen = not character_die_slot.is_frozen
-    # depending on result, we want to show this result as "locked" or "loose"
+    if character_die_slot.is_frozen:
+        frozen_background.show()
+        frozen_icon.show()
+    else:
+        frozen_background.hide()
+        frozen_icon.hide()
+
+func _handle_freeze_roll_action(event: InputEvent):
+    if event.is_action_pressed("freeze_roll"):
+        toggle_freeze()
+        get_viewport().set_input_as_handled()
