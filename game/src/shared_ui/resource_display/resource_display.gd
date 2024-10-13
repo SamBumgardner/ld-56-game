cb107127a4961ff7844a07f3a18bcea789cf4c74
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-13 12:44:07 -0500

    added configurable delay and made just the label shake on error

--- game/src/shared_ui/resource_display/resource_display.gd
+++ game/src/shared_ui/resource_display/resource_display.gd
@@ -5,6 +5,7 @@ const MIN_DURATION: float = .1
 const COLOR_CHANGE_DURATION: float = 2
 
 @export var unit_change_per_second: int = 30
+@export var resource_change_start_delay: float = 0
 @export var COLOR_INCREASE: Color = Color.GREEN
 @export var COLOR_DECREASE: Color = Color.DARK_ORANGE
 @export var COLOR_INSUFFICIENT: Color = Color.RED
@@ -12,7 +13,7 @@ const COLOR_CHANGE_DURATION: float = 2
 @onready var database: Database = $"/root/Database"
 @onready var amount_label: Label = $MarginContainer/HBoxContainer/Amount
 
-var start_position: Vector2
+var label_start_position: Vector2
 var current_display_value: int:
     set(new_value):
         current_display_value = new_value
@@ -24,13 +25,13 @@ var position_tween: Tween
 ## children are responsible for connecting "value_changed" events and setting inital amount
 func _ready() -> void:
     await get_tree().process_frame
-    start_position = position
+    label_start_position = amount_label.position
 
 func force_display_resolution():
     if value_tween != null and value_tween.is_running():
-        value_tween.custom_step(MAX_DURATION)
+        value_tween.custom_step(resource_change_start_delay + MAX_DURATION)
     if color_tween != null and color_tween.is_running():
-        color_tween.custom_step(COLOR_CHANGE_DURATION)
+        color_tween.custom_step(resource_change_start_delay + COLOR_CHANGE_DURATION)
     if position_tween != null and position_tween.is_valid():
         position_tween.custom_step(MAX_DURATION)
 
@@ -43,10 +44,10 @@ func _create_position_tween():
         position_tween.stop()
     position_tween = create_tween()
     position_tween.tween_method(shake_display, 0, 0, .1)
-    position_tween.tween_property(self, "position", start_position, 0)
+    position_tween.tween_property(amount_label, "position", label_start_position, 0)
 
 func shake_display(_value: float):
-    position = start_position + (Vector2.ONE.rotated(randf_range(0, 6.29)) * 5)
+    amount_label.position = label_start_position + (Vector2.ONE.rotated(randf_range(0, 6.29)) * 5)
 
 func _on_resource_changed(new_value, old_value) -> void:
     _set_up_value_tween(new_value)
@@ -61,6 +62,7 @@ func _set_up_color_tween(color: Color) -> void:
     color_tween = create_tween()
     color_tween.set_ease(Tween.EASE_OUT)
     color_tween.set_trans(Tween.TRANS_CUBIC)
+    color_tween.tween_interval(resource_change_start_delay)
     color_tween.tween_property(amount_label, "modulate", Color.WHITE, COLOR_CHANGE_DURATION)
 
 func _set_up_value_tween(new_money_value) -> void:
@@ -72,6 +74,7 @@ func _set_up_value_tween(new_money_value) -> void:
     value_tween = create_tween()
     value_tween.set_ease(Tween.EaseType.EASE_OUT)
     value_tween.set_trans(Tween.TransitionType.TRANS_CUBIC)
+    value_tween.tween_interval(resource_change_start_delay)
     value_tween.tween_property(self, "current_display_value", new_money_value, duration)
     
 func _calculate_value_tween_duration(old_value: int, new_value: int) -> float:
