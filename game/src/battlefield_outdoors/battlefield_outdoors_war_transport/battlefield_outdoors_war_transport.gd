f494d6526b0013c8eae5977a53d27bdd0f19410d
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-13 13:26:44 -0500

    add simple movement tweening to transport

--- game/src/battlefield_outdoors/battlefield_outdoors_war_transport/battlefield_outdoors_war_transport.gd
+++ game/src/battlefield_outdoors/battlefield_outdoors_war_transport/battlefield_outdoors_war_transport.gd
@@ -1,1 +1,19 @@
 class_name BattlefieldOutdoorsWarTransport extends Sprite2D
+
+@onready var start_position = position
+
+var movement_tween: Tween
+
+func charge_at_target(target_position: Vector2, duration: float) -> void:
+    movement_tween = clear_tween(movement_tween)
+    movement_tween.tween_property(self, "position", target_position, duration)
+
+func return_to_start_position(duration: float) -> void:
+    movement_tween = clear_tween(movement_tween)
+    movement_tween.tween_property(self, "position", start_position, duration)
+
+func clear_tween(tween: Tween) -> Tween:
+    if tween != null and tween.is_running():
+        tween.kill()
+    
+    return create_tween()
