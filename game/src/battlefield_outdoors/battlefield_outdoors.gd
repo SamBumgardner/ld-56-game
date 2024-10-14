eaa5df489615a347639c457370fd626d9141e078
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-14 18:26:16 -0500

    implement free reroll after charge completed

--- game/src/battlefield_outdoors/battlefield_outdoors.gd
+++ game/src/battlefield_outdoors/battlefield_outdoors.gd
@@ -126,8 +126,11 @@ func _on_charge_cooldown(duration: float) -> void:
     war_transport.hide_power(duration)
     war_transport.return_to_start_position(duration)
     _generate_and_scale_next_barrier()
+    # clear all dice frozen status, put display in rerolling prep
 
 func _on_charge_finish() -> void:
+    const free_reroll_cost = 0
+    _on_roll_requested(free_reroll_cost)
     if _should_save_checkpoint():
         _save_checkpoint()
     combat_result.clear()
@@ -184,14 +187,15 @@ func _on_health_empty() -> void:
     game_over_sequence.tween_callback(
         get_tree().change_scene_to_packed.bind(preload("res://src/game_over/game_over.tscn")))
 
-func _on_roll_requested():
-    if _has_enough_fuel():
+func _on_roll_requested(override_cost: int = -1):
+    var roll_cost: int = override_cost if override_cost >= 0 else Database.current_reroll_fuel_cost
+    if _has_enough_fuel(roll_cost):
         dice_roll_started.emit()
-        Database.set_fuel(Database.current_fuel - Database.current_reroll_fuel_cost)
+        Database.set_fuel(Database.current_fuel - roll_cost)
         _roll_dice()
 
-func _has_enough_fuel() -> bool:
-    var enough_fuel: bool = Database.current_fuel >= Database.current_reroll_fuel_cost
+func _has_enough_fuel(cost: int) -> bool:
+    var enough_fuel: bool = Database.current_fuel >= cost
     if not enough_fuel:
         insufficient_fuel.emit()
     return enough_fuel
