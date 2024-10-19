3ad746154577c5dc6a241660b278af5d593e490c
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-19 15:38:48 -0500

    add custom hover info to region display

--- game/src/shared_ui/region_display.gd
+++ game/src/shared_ui/region_display.gd
@@ -1,12 +1,16 @@
 class_name RegionDisplay extends PanelContainer
 
 @onready var region_name_label: Label = $MarginContainer/VBoxContainer/RegionName
+@onready var region_info_container: Container = $MarginContainer/VBoxContainer/RegionInfoContainer
+@onready var region_info_label: RichTextLabel = $MarginContainer/VBoxContainer/RegionInfoContainer/MarginContainer/RegionInfo
 
 var fade_tween: Tween
 
 func _ready() -> void:
     Database.region_changed.connect(_on_region_changed)
-    _update_name_label_text(Database.current_region.region_name)
+    _update_region_information(Database.current_region)
+    mouse_entered.connect(_on_mouse_entered)
+    mouse_exited.connect(_on_mouse_exited)
 
 func _on_region_changed(new_region: Region):
     if region_name_label.text != new_region.region_name:
@@ -17,10 +21,18 @@ func _on_region_changed(new_region: Region):
                 fade_tween.kill()
             fade_tween = create_tween()
             fade_tween.tween_property(self, "modulate", Color.TRANSPARENT, duration)
-            fade_tween.tween_callback(_update_name_label_text.bind(new_region.region_name))
+            fade_tween.tween_callback(_update_region_information.bind(new_region.region_name))
             fade_tween.tween_property(self, "modulate", Color.WHITE, duration)
         else:
-            _update_name_label_text(new_region.region_name)
+            _update_region_information(new_region)
 
-func _update_name_label_text(new_name: String):
-    region_name_label.text = new_name
+func _update_region_information(region: Region):
+    region_name_label.text = region.region_name
+    region_info_label.text = "Region Info:\n%s" % region.gameplay_description
+
+func _on_mouse_entered():
+    region_info_container.show()
+
+func _on_mouse_exited():
+    region_info_container.hide()
+    
