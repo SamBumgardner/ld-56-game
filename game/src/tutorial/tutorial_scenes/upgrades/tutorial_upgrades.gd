d3cc9a46a3019d4d3917e7ab86ec61b1d5e0bcbf
Sam Bumgardner <Sambumgardner1@gmail.com>
2024-10-20 13:51:59 -0500

    finish indoors tutorial

--- game/src/tutorial/tutorial_scenes/upgrades/tutorial_upgrades.gd
+++ game/src/tutorial/tutorial_scenes/upgrades/tutorial_upgrades.gd
@@ -11,5 +11,14 @@ func _ready() -> void:
     type_to_trigger_map["GoOutsideButton"] = [
         TutorialTrigger.new("pressed", _on_trigger_received.bind("go_outside")),
     ]
+    type_to_trigger_map["CrewMemberDetail"] = [
+        TutorialTrigger.new("visibility_changed", _on_trigger_received.bind("character_detail_open_close")),
+    ]
+    type_to_trigger_map["UpgradeChoiceDisplay"] = [
+        TutorialTrigger.new("upgrade_selected", func(_a, _b, _c): _on_trigger_received("upgrade_selected")),
+    ]
+    type_to_trigger_map["UpgradeCostPrompt"] = [
+        TutorialTrigger.new("upgrade_purchase_pressed", func(_a): _on_trigger_received("upgrade_purchased")),
+    ]
     super()
     show()
